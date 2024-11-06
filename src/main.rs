1ca77c5188b72a068bb63c2227139b3ba8450229
buttoncorporation <legal@buttoncorp.org>
2024-11-06 02:24:40 -0500

    configuring CORS

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,39 @@ use chrono::Utc;
 use std::time::{UNIX_EPOCH, Duration};
 #[macro_use] extern crate rocket;
 
+// FARING for CORS
+use rocket::http::Header;
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "buttoncorp.org"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
+
+
+
+
+
+
+
+
+
 // Main home page (will also be processed as 404 page)
 // nothing complex
 #[get("/")]
